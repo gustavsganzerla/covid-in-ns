42602a15c01f17cefae28f3a20149d5c81d8403b
gustavsganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2022-01-27 09:24:56 -0300

    Update vaccinations.R

--- vaccinations.R
+++ vaccinations.R
@@ -1,8 +1,6 @@
 recent_ns <- covid_ns[covid_ns$data...date>=as.Date("2021-10-01"),]
 
-covid_ns$data...change_cases[covid_ns$data...change_cases<0] <- 0
-recent_ns$data...change_hospitalizations[recent_ns$data...change_hospitalizations<0] <- 0
-recent_ns$data...change_criticals[recent_ns$data...change_criticals<0] <- 0
+
 
 cases_hosps_ns <- ggplot()+
   geom_line(data=recent_ns, aes(x=as.Date(data...date), y=data...change_hospitalizations, group=1, color = "hospitalizations"))+
@@ -14,6 +12,5 @@ cases_hosps_ns <- ggplot()+
   ggtitle("B")+
   scale_x_date(date_labels = "%Y-%m-%d", breaks = "10 days")
 
-
-ggarrange(cases_ns, cases_hosps_ns, ncol = 1)            
+           
             
